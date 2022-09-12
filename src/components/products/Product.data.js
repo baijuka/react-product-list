a97395e627a7356f31a807bf3e3cbe040ce64adb
Baiju Abraham <baijuka@yahoo.com>
2022-09-12 18:27:13 +0100

    Add pagination

--- src/components/products/Product.data.js
+++ src/components/products/Product.data.js
@@ -4,7 +4,7 @@ export const productData = [
         model: 'Dolphin Bubble Roll- Yellow Border',
         desc: 'Yellow Border with Rubber Compound enhances durability.',
         price: 25.00,
-        image: 'IMG_RMDRS622DYR',
+        image: 'IMG29',
         qty: 1
     },
     {
@@ -12,7 +12,7 @@ export const productData = [
         model: 'Dolphin Bubble Rolls',
         desc: 'Unique bubble surface design reduces fatigue from standing and molded rubber bubble on top and bottom of mats ensures comfort and easy air flow.',
         price: 20.00,
-        image: 'IMG_RMDRS622ER0',
+        image: 'IMG1',
         qty: 1
     },
     {
@@ -20,7 +20,7 @@ export const productData = [
         model: 'Dolphin Comfort Rolls',
         desc: 'An aggressive top surface that provides premium traction and drainage abilities.',
         price: 18.00,
-        image: 'IMG_RMDRH125A00',
+        image: 'IMG2',
         qty: 1
     },
     {
@@ -28,7 +28,7 @@ export const productData = [
         model: 'Dolphin O-Ring Rolls',
         desc: 'Rubber Runners work well on hard floor and can be used near ice rinks.',
         price: 20.00,
-        image: 'IMG_RMDRH101YR0',
+        image: 'IMG3',
         qty: 1
     },
     {
@@ -36,7 +36,247 @@ export const productData = [
         model: 'Dolphin Stripin Rolls & Giant Stripin Mat',
         desc: 'Stripin Rolls are an excellent exterior doormat to act as a first line of defense against dirt and debris entering your building.',
         price: 22.00,
-        image: 'IMG_RMDRP229A00',
+        image: 'IMG4',
+        qty: 1
+    },
+    {
+        id: 6,
+        model: 'Coco Brush Mats',
+        desc: 'These are heavy-duty coir mats, molded into a rectangular or half –moon shaped rubber body. The rubber backing provides non-slip property, and the coir ridges ensure excellent scraping and absorption property to the mats.',
+        price: 13.90,
+        image: 'IMG5',
+        qty: 1
+    },
+    {
+        id: 7,
+        model: 'Thooral matt',
+        desc: 'A best mat for you to thoor freely',
+        price: 22.00,
+        image: 'IMG6',
+        qty: 1
+    },
+    {
+        id: 8,
+        model: 'zzz',
+        desc: 'zzz',
+        price: 22.00,
+        image: 'IMG7',
+        qty: 1
+    },
+    {
+        id: 9,
+        model: 'zzz',
+        desc: 'zzzz',
+        price: 22.00,
+        image: 'IMG8',
+        qty: 1
+    },
+    {
+        id: 10,
+        model: 'zzzzz',
+        desc: 'zzzzz',
+        price: 22.00,
+        image: 'IMG9',
+        qty: 1
+    },
+    {
+        id: 11,
+        model: 'zzzzz',
+        desc: 'zzzzz',
+        price: 22.00,
+        image: 'IMG10',
+        qty: 1
+    },
+    {
+        id: 12,
+        model: 'zzzzz',
+        desc: 'zzzz',
+        price: 22.00,
+        image: 'IMG11',
+        qty: 1
+    },
+    {
+        id: 13,
+        model: 'zzzzz',
+        desc: 'zzzzzz',
+        price: 22.00,
+        image: 'IMG12',
+        qty: 1
+    },
+    {
+        id: 14,
+        model: 'zzzzz',
+        desc: 'zzzzzzzzzzzz',
+        price: 22.00,
+        image: 'IMG13',
+        qty: 1
+    },
+    {
+        id: 15,
+        model: 'zzzzzzzz',
+        desc: 'zzzzzzzzzzzzz',
+        price: 22.00,
+        image: 'IMG14',
+        qty: 1
+    },
+    {
+        id: 16,
+        model: 'zzzzzz',
+        desc: 'zzzzzzzzzzzz',
+        price: 22.00,
+        image: 'IMG15',
+        qty: 1
+    },
+    {
+        id: 17,
+        model: 'zzzzzz',
+        desc: 'zzzzzzzzzzz',
+        price: 22.00,
+        image: 'IMG16',
+        qty: 1
+    },
+    {
+        id: 18,
+        model: 'zzzz',
+        desc: 'zzzzzzzzzzz',
+        price: 22.00,
+        image: 'IMG17',
+        qty: 1
+    },
+    {
+        id: 19,
+        model: 'zzzz',
+        desc: 'zzzzzz',
+        price: 22.00,
+        image: 'IMG18',
+        qty: 1
+    },
+    {
+        id: 20,
+        model: 'dddd',
+        desc: 'dddddd',
+        price: 22.00,
+        image: 'IMG19',
+        qty: 1
+    },
+    {
+        id: 21,
+        model: 'dddddd',
+        desc: 'ddddddd',
+        price: 22.00,
+        image: 'IMG20',
+        qty: 1
+    },
+    {
+        id: 22,
+        model: 'fffff',
+        desc: 'fffff',
+        price: 22.00,
+        image: 'IMG21',
+        qty: 1
+    },
+    {
+        id: 23,
+        model: 'fffff',
+        desc: 'ffffffff',
+        price: 22.00,
+        image: 'IMG22',
+        qty: 1
+    },
+    {
+        id: 24,
+        model: 'tttt',
+        desc: 'tttttt',
+        price: 22.00,
+        image: 'IMG23',
+        qty: 1
+    },
+    {
+        id: 25,
+        model: 'rrrr',
+        desc: 'rrrrrrrrr',
+        price: 22.00,
+        image: 'IMG24',
+        qty: 1
+    },
+    {
+        id: 26,
+        model: 'qqq',
+        desc: 'qqqqqqqqq',
+        price: 22.00,
+        image: 'IMG25',
+        qty: 1
+    },
+    {
+        id: 27,
+        model: 'ttt',
+        desc: 'ttttttttt',
+        price: 22.00,
+        image: 'IMG26',
+        qty: 1
+    },
+    {
+        id: 28,
+        model: 'uuu',
+        desc: 'uuuuuuuuu',
+        price: 22.00,
+        image: 'IMG27',
+        qty: 1
+    },
+    {
+        id: 29,
+        model: 'jjj',
+        desc: 'jjjjjjjjj',
+        price: 22.00,
+        image: 'IMG28',
+        qty: 1
+    },
+    {
+        id: 30,
+        model: 'yuuu',
+        desc: 'kkkkk',
+        price: 22.00,
+        image: 'IMG35',
+        qty: 1
+    },
+    {
+        id: 31,
+        model: 'kkkkk',
+        desc: 'kkkkk',
+        price: 22.00,
+        image: 'IMG30',
+        qty: 1
+    },
+    {
+        id: 32,
+        model: 'kkkk',
+        desc: 'kkkkkkkk',
+        price: 22.00,
+        image: 'IMG31',
+        qty: 1
+    },
+    {
+        id: 33,
+        model: 'kkkkk',
+        desc: 'jjjjjjjjj',
+        price: 22.00,
+        image: 'IMG32',
+        qty: 1
+    },
+    {
+        id: 34,
+        model: 'jjjjj',
+        desc: 'jghjhghjhg',
+        price: 22.00,
+        image: 'IMG33',
+        qty: 1
+    },
+    {
+        id: 35,
+        model: 'jjjghj',
+        desc: 'jgjghj',
+        price: 22.00,
+        image: 'IMG34',
         qty: 1
     },
 ]
--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -1,9 +1,21 @@
-import React, { useState } from 'react';
+import ReactPaginate from 'react-paginate';
+import React, { useState, useEffect } from 'react';
 import {productData} from './Product.data';
 import {Card, Button} from 'react-bootstrap';
 
 const Products = () => {
-    const [items, setItems] = useState(productData);
+    const [items, setItems] = useState([]);
+    const totalItems = productData.length;
+    let limit = 8;
+    const [pageCount] = useState(Math.ceil(totalItems / limit));
+    let currentPage = 1;
+    
+    // setPageCount(Math.ceil(totalItems / limit));
+
+    useEffect(()=> {
+        const getFirstData = productData.slice(0, limit)
+        setItems(getFirstData);
+    },[])
 
     const decQty = (id)=> {
         const newItem = items.map((item)=> 
@@ -17,12 +29,28 @@ const Products = () => {
         );
         setItems(newItem);
     }
+
+    const fetchItems = () => {
+        const indexStart = currentPage * limit - limit;
+        const indexEnd = currentPage* limit
+        console.log(indexStart, indexEnd);
+        const data = productData.slice(indexStart, indexEnd)
+        return data
+    }
+
+    const handlePageClick = (data) => {
+        currentPage = data.selected + 1
+        const pageItems =  fetchItems(currentPage)
+
+        setItems(pageItems)
+    }
+
   return (
     <div>
         <h1 className='bg-info text-white'>Product List</h1>
         {items.map((item)=>(
             <div className='d-inline-flex' key={item.id}>
-                <Card className="shadow p-3 m-2 bg-white rounded" style={{ width: '18rem', height: '35rem' }}>
+                <Card className="shadow p-3 m-2 bg-white rounded" style={{ width: '18rem', height: '38rem' }}>
                 <Card.Img className='p-2'  style={{ width: '100%', height: '10rem' }} variant="top" src={require(`./assets/${item.image}.jpg`)} />
                 <Card.Body>
                     <Card.Title className="text-primary">{item.model}</Card.Title>
@@ -39,6 +67,25 @@ const Products = () => {
             </div>
         ))}
         
+        <ReactPaginate 
+            previousLabel = {'Previous'}
+            nextLabel = {'Next'}
+            breakLabel = {'...'}
+            pageCount = {pageCount}
+            marginPagesDisplayed = {3}
+            pageRangeDisplayed = {3}
+            onPageChange = {handlePageClick}
+            containerClassName = {'pagination justify-content-center'}
+            pageClassName = {'page-item'}
+            pageLinkClassName= {'page-link'}
+            previousClassName = {'page-item'}
+            previousLinkClassName = {'page-link'}
+            nextClassName = {'page-item'}
+            nextLinkClassName = {'page-link'}
+            breakClassName = {'page-item'}
+            breakLinkClassName = {'page-link'}
+            activeClassName = {'active'}
+        />
     </div>
   );
 }
